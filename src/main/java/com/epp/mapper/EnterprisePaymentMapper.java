2f27f4e761b1051f0211e393b852363ff0041a18
Azusayume <zhangzimeng_5962@163.com>
2022-12-25 20:14:19 +0800

    Revert "commit"
    This reverts commit 9fb614ca6acb5bb0da63b3acae8795f68819b067.

--- src/main/java/com/epp/mapper/EnterprisePaymentMapper.java
+++ src/main/java/com/epp/mapper/EnterprisePaymentMapper.java
@@ -3,9 +3,12 @@ package com.epp.mapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.epp.pojo.EnterprisePayment;
+import com.epp.pojo.EnterprisePaymentExample;
 import java.util.List;
+import org.apache.ibatis.annotations.Param;
 
-import org.apache.ibatis.annotations.*;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface EnterprisePaymentMapper {
@@ -13,43 +16,28 @@ public interface EnterprisePaymentMapper {
 
     //int deleteByExample(EnterprisePaymentExample example);
 
-    @Delete("delete from enterprise_payment where enterprise_payment_id = #{enterprisePaymentId,jdbcType=INTEGER}")
     int deleteByPrimaryKey(Integer enterprisePaymentId);
 
-    @Insert("  insert into enterprise_payment (enterprise_id, payment, year, \n" +
-            "      month, status)\n" +
-            "    values (#{enterpriseId,jdbcType=VARCHAR}, #{payment,jdbcType=VARCHAR}, #{year,jdbcType=INTEGER}, \n" +
-            "      #{month,jdbcType=INTEGER}, #{status,jdbcType=INTEGER})")
     int insert(EnterprisePayment record);
 
-//    int insertSelective(EnterprisePayment record);
+    int insertSelective(EnterprisePayment record);
 
-//    List<EnterprisePayment> selectByExample(EnterprisePaymentExample example);
+    List<EnterprisePayment> selectByExample(EnterprisePaymentExample example);
 
-    @Select(" select \n" +
-            "    from enterprise_payment\n" +
-            "    where enterprise_payment_id = #{enterprisePaymentId,jdbcType=INTEGER}")
     EnterprisePayment selectByPrimaryKey(Integer enterprisePaymentId);
 
-    @Select("select * from enterprise_payment")
-    List<EnterprisePayment> selectAll();
+    @Select("select * from ")
+    List<> selectAll();
 
-    @Select("select * from enterprise_payment")
-    IPage<EnterprisePayment> selectAll(Page page);
+    @Select("select * from ")
+    IPage<> selectAll(Page page);
 
 
     ////int updateByExampleSelective(@Param("record") EnterprisePayment record, @Param("example") EnterprisePaymentExample example);
 
     //int updateByExample(@Param("record") EnterprisePayment record, @Param("example") EnterprisePaymentExample example);
 
-//    int updateByPrimaryKeySelective(EnterprisePayment record);
+    int updateByPrimaryKeySelective(EnterprisePayment record);
 
-    @Update("  update enterprise_payment\n" +
-            "    set enterprise_id = #{enterpriseId,jdbcType=VARCHAR},\n" +
-            "      payment = #{payment,jdbcType=VARCHAR},\n" +
-            "      year = #{year,jdbcType=INTEGER},\n" +
-            "      month = #{month,jdbcType=INTEGER},\n" +
-            "      status = #{status,jdbcType=INTEGER}\n" +
-            "    where enterprise_payment_id = #{enterprisePaymentId,jdbcType=INTEGER}")
     int updateByPrimaryKey(EnterprisePayment record);
 }
